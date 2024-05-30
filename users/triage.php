43bbcf859a8b3b37d08f0b65251439c3629171a1
Nicole <nicolebosibori74@gmail.com>
2024-05-30 10:07:54 +0300

    updated

--- users/triage.php
+++ users/triage.php
@@ -1,144 +1,115 @@
 <?php
 session_start();
 include('config.php');
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Triage</title>
+    <link rel="stylesheet" href="managepatients.css">
+</head>
 <body>
-        <div class="content-page">
-            <div class="content">
+    <header class="navbar">
+        <div class="container text-center">
+            <h1 style="margin: 0; font-size: 24px; color: #ffc300;">CATHOLIC UNIVERSITY OF EASTERN AFRICA</h1>
+        </div>
+    </header>
 
-                <!-- Start Content-->
-                <div class="container-fluid">
+    <div class="content-page">
+        <div class="content">
 
-                    <!-- start page title -->
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="page-title-box">
-                                <div class="page-title-right">
-                                    <ol class="breadcrumb m-0">
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                                        <li class="breadcrumb-item"><a href="javascript: void(0);">Triage</a></li>
-                                        <li class="breadcrumb-item active">Capture Vitals</li>
-                                    </ol>
-                                </div>
-                                <h4 class="page-title">Capture Patient Vitals</h4>
+            <!-- Start Content-->
+            <div class="container-fluid">
+
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box">
+                            <div class="page-title-right">
+                                <ol class="breadcrumb m-0">
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Triage</a></li>
+                                    <li class="breadcrumb-item active">Records</li>
+                                </ol>
                             </div>
                         </div>
                     </div>
-                    <!-- end page title -->
+                </div>
+                <!-- end page title -->
 
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card-box">
-                                <h4 class="header-title"></h4>
-                                <div class="mb-2">
-                                    <div class="row">
-                                        <div class="col-12 text-sm-center form-inline">
-                                            <div class="form-group mr-2" style="display:none">
-                                                <select id="demo-foo-filter-status" class="custom-select custom-select-sm">
-                                                    <option value="">Show all</option>
-                                                    <option value="Discharged">Discharged</option>
-                                                    <option value="OutPatients">OutPatients</option>
-                                                    <option value="InPatients">InPatients</option>
-                                                </select>
-                                            </div>
-                                            <div class="form-group">
-                                                <input id="demo-foo-search" type="text" placeholder="Search" class="form-control form-control-sm" autocomplete="on">
-                                            </div>
-                                        </div>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card-box">
+                            <h4 class="header-title">Patient Records</h4>
+                            <div class="mb-2">
+                                <div class="row">
+                                    <div class="col-12 text-sm-center form-inline">
                                     </div>
                                 </div>
+                            </div>
 
-                                <div class="table-responsive">
-                                    <table id="demo-foo-filtering" class="table table-bordered toggle-circle mb-0" data-page-size="7">
-                                        <thead>
+                            <div class="table-responsive">
+                                <table id="demo-foo-filtering" class="table table-bordered toggle-circle mb-0" data-page-size="7">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th data-toggle="true">Patient Name</th>
+                                            <th data-hide="phone">Patient ID</th>
+                                            <th data-hide="phone">Contact Number</th>
+                                            <th data-hide="phone">Address</th>
+                                            <th data-hide="phone">Age</th>
+                                            <th data-hide="phone">Gender</th>
+                                            <th data-hide="phone">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <?php
+                                    $ret = "SELECT * FROM patients ORDER BY created_at DESC";
+                                    $stmt = $mysqli->prepare($ret);
+                                    $stmt->execute();
+                                    $res = $stmt->get_result();
+                                    $cnt = 1;
+                                    while ($row = $res->fetch_object()) {
+                                    ?>
+                                        <tbody>
                                             <tr>
-                                                <th>#</th>
-                                                <th data-toggle="true">Patient Name</th>
-                                                <th data-hide="phone">Patient ID</th>
-                                                <th data-hide="phone">Contact Number</th>
-                                                <th data-hide="phone">Address</th>
-                                                <th data-hide="phone">Age</th>
-                                                <th data-hide="phone">Gender</th>
-                                                <th data-hide="phone">Action</th>
+                                                <td><?php echo $cnt; ?></td>
+                                                <td><?php echo $row->name; ?></td>
+                                                <td><?php echo $row->patient_id; ?></td>
+                                                <td><?php echo $row->contact_number; ?></td>
+                                                <td><?php echo $row->address; ?></td>
+                                                <td><?php echo $row->age; ?> Years</td>
+                                                <td><?php echo $row->gender; ?></td>
+                                                <td><a href="capture_vitals.php?patient_id=<?php echo $row->patient_id; ?>" class="badge badge-success"><i class="mdi mdi-beaker"></i> Capture Vitals</a></td>
                                             </tr>
-                                        </thead>
-                                        <?php
-                                        $ret = "SELECT * FROM patients ORDER BY created_at DESC";
-                                        $stmt = $mysqli->prepare($ret);
-                                        $stmt->execute();
-                                        $res = $stmt->get_result();
-                                        $cnt = 1;
-                                        while ($row = $res->fetch_object()) {
-                                        ?>
+                                        </tbody>
+                                    <?php $cnt = $cnt + 1; } ?>
+                                    <tfoot>
+                                        <tr class="active">
+                                            <td colspan="8">
+                                                <div class="text-right">
+                                                    <ul class="pagination pagination-rounded justify-content-end footable-pagination m-t-10 mb-0"></ul>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div> <!-- end .table-responsive-->
+                        </div> <!-- end card-box -->
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
 
-                                            <tbody>
-                                                <tr>
-                                                    <td><?php echo $cnt; ?></td>
-                                                    <td><?php echo $row->name; ?></td>
-                                                    <td><?php echo $row->patient_id; ?></td>
-                                                    <td><?php echo $row->contact_number; ?></td>
-                                                    <td><?php echo $row->address; ?></td>
-                                                    <td><?php echo $row->age; ?> Years</td>
-                                                    <td><?php echo $row->gender; ?></td>
+            </div> <!-- container -->
 
-                                                    <td><a href="capture_vitals.php?patient_id=<?php echo $row->patient_id; ?>" class="badge badge-success"><i class="mdi mdi-beaker"></i> Capture Vitals</a></td>
-                                                </tr>
-                                            </tbody>
-                                        <?php $cnt = $cnt + 1;
-                                        } ?>
-                                        <tfoot>
-                                            <tr class="active">
-                                                <td colspan="8">
-                                                    <div class="text-right">
-                                                        <ul class="pagination pagination-rounded justify-content-end footable-pagination m-t-10 mb-0"></ul>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        </tfoot>
-                                    </table>
-                                </div> <!-- end .table-responsive-->
-                            </div> <!-- end card-box -->
-                        </div> <!-- end col -->
-                    </div>
-                    <!-- end row -->
+        </div> <!-- content -->
 
-                </div> <!-- container -->
+        <footer class="footer">
+            <div class="container">
+                <p style="margin: 0;">&copy; 2024 Catholic University of Eastern Africa</p>
+            </div>
+        </footer>
 
-            </div> <!-- content -->
-
-            <!-- Footer Start -->
-            <?php include('assets/inc/footer.php'); ?>
-            <!-- end Footer -->
-
-        </div>
-
-        <!-- ============================================================== -->
-        <!-- End Page content -->
-        <!-- ============================================================== -->
-
-    </div>
-    <!-- END wrapper -->
-
-    <!-- Right bar overlay-->
-    <div class="rightbar-overlay"></div>
-
-    <!-- Vendor js -->
-    <script src="assets/js/vendor.min.js"></script>
-
-    <!-- Footable js -->
-    <script src="assets/libs/footable/footable.all.min.js"></script>
-
-    <!-- Init js -->
-    <script src="assets/js/pages/foo-tables.init.js"></script>
-
-    <!-- App js -->
-    <script src="assets/js/app.min.js"></script>
-
-</body>
-
-</html>
+    </div
