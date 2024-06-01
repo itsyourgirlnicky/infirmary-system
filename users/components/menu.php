5c6593e1776e72fe4d53f4c89859a1abaaf83b38
Snow <onyangogeorge954@gmail.com>
2024-06-01 12:14:59 +0300

    chore: create sidenavigation bar

---
+++ users/components/menu.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="menu.css">
+</head>
+<body>
+    <section class="app">
+        <aside class="sidebar">
+               <header>
+              Menu
+            </header>
+          <nav class="sidebar-nav">
+       
+            <ul>
+              <li>
+                <a href="#"><i class="ion-bag"></i> <span>Patient Registration</span></a>
+                <ul class="nav-flyout">
+                  <li>
+                    <a href="#"><i class="ion-ios-color-filter-outline"></i>Add Patient Details</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-clock-outline"></i>Manage Patients</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#"><i class="ion-ios-settings"></i> <span class="">Triage</span></a>
+                <ul class="nav-flyout">
+                  <li>
+                    <a href="#"><i class="ion-ios-alarm-outline"></i>Watch</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-camera-outline"></i>Creeper</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-chatboxes-outline"></i>Hate</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-cog-outline"></i>Grinder</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#"><i class="ion-ios-briefcase-outline"></i> <span class="">Consultation</span></a>
+                <ul class="nav-flyout">
+                  <li>
+                    <a href="#"><i class="ion-ios-flame-outline"></i>Burn</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-lightbulb-outline"></i>Bulbs</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-location-outline"></i>Where You</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-locked-outline"></i>On Lock</a>
+                  </li>
+                   <li>
+                    <a href="#"><i class="ion-ios-navigate-outline"></i>Ghostface</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#"><i class="ion-ios-analytics-outline"></i> <span class="">Laboratory</span></a>
+                <ul class="nav-flyout">
+                  <li>
+                    <a href="#"><i class="ion-ios-timer-outline"></i>Timers</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-arrow-graph-down-left"></i>You Lose</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-partlysunny-outline"></i>Stormy</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-timer-outline"></i>Lookie Look</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-game-controller-a-outline"></i>Dork Mfer</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#"><i class="ion-ios-paper-outline"></i> <span class="">Pharmacy</span></a>
+                <ul class="nav-flyout">
+                  <li>
+                    <a href="#"><i class="ion-ios-filing-outline"></i>File Cab</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-information-outline"></i>Infos</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-paperplane-outline"></i>Planes</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-android-star-outline"></i>Shop</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#"><i class="ion-ios-navigate-outline"></i> <span class="">Inventory</span></a>
+                <ul class="nav-flyout">
+                  <li>
+                    <a href="#"><i class="ion-ios-flame-outline"></i>Burn</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-lightbulb-outline"></i>Bulbs</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-location-outline"></i>Where You</a>
+                  </li>
+                  <li>
+                    <a href="#"><i class="ion-ios-locked-outline"></i>On Lock</a>
+                  </li>
+                   <li>
+                    <a href="#"><i class="ion-ios-navigate-outline"></i>Ghostface</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#"><i class="ion-ios-medical-outline"></i> <span class="">Billing</span></a>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+      </section>
+</body>
+</html>
