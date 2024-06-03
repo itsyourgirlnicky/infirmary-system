1c0f863bef5817a378f7cac3716204b0aa0fb34d
Snow <onyangogeorge954@gmail.com>
2024-06-03 10:44:16 +0300

    chore: update top navigation look

--- users/components/navbar.php
+++ users/components/navbar.php
@@ -1,13 +1,17 @@
-<?php  ?>
-<div style="background-color: #ffc300; width: 100%;">
-  <div style="display: flex; flex-direction: row; width: 100%">
-    <ul style="list-style-type: none; display: flex; flex-direction: row; justify-content: end; width: 100%; gap: 2rem; margin-right: 1rem;">
-      <li>  <a style="text-decoration: none; color: white; font-size: 16px;" href="../dashboard.php">Home</a>
-      </li>
-      <li>  <a style="text-decoration: none; color: white; font-size: 16px;" href="">Profile</a>
-      </li>
-      <li>  <a style="text-decoration: none; color: white; font-size: 16px;" href="">Logout</a>
-      </li>
-    </ul>
-  </div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="navbar.css">
+  </head>
+  <body>
+    <nav>
+      <a href="#">Home</a>
+      <a href="#">Profile</a>
+      <a href="#">Logout</a>
+      <div class="animation start-home"></div>
+    </nav>
+  </body>
+</html>
